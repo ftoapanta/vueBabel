a5eec1617ae9b5459521d82d49b9a25e42e37512
Freddy Toapanta <freddy.toapanta@planmarket.net>
2018-11-22 16:23:12 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 var saludar = function saludar(nombre) {
   return console.log(nombre);
 };
 
 var despedir = function despedir(nombre) {
   return console.log(nombre);
-}; 
+};
+
+var renunciar = function renunciar() {
+  return console.log('error');
+};
+
+var Persona =
+/*#__PURE__*/
+function () {
+  function Persona(nombre) {
+    _classCallCheck(this, Persona);
+
+    this.nombre = nombre;
+  }
+
+  _createClass(Persona, [{
+    key: "caminar",
+    value: function caminar() {
+      console.log('Estoy caminando');
+    }
+  }]);
+
+  return Persona;
+}();
--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
 let saludar = nombre => console.log(nombre);
 let despedir = nombre => console.log(nombre);
+let renunciar = () => console.log('error');
+
+class Persona
+{
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    caminar()
+    {
+        console.log('Estoy caminando');
+    }
+}
